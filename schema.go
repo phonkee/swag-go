35c5db814645837cedc12aa30feb1988b1d90bc6
Peter Vrba <phonkee@phonkee.eu>
2021-10-25 01:42:22 +0200

    working

--- schema.go
+++ schema.go
@@ -1,6 +1,7 @@
 package swag
 
 import (
+	"errors"
 	"reflect"
 	"sync"
 
@@ -8,11 +9,16 @@ import (
 )
 
 var (
-	schemaReg = newSchemaRegistry()
+	schemaReg              = newSchemaRegistry()
+	errSchemaValueNotFound = errors.New("not found schema")
 )
 
-func registerSchema(target interface{}, fn func() (*spec.Schema, error)) error {
+func registerSchema(target interface{}, fn func(*schemaRegistry, interface{}) (*spec.Schema, error)) error {
 	return schemaReg.registerSchema(target, fn)
+}
+
+func registerSchemaKind(target interface{}, fn func(*schemaRegistry, interface{}) (*spec.Schema, error)) error {
+	return nil
 }
 
 func getSchema(target interface{}, defs spec.Definitions) (*spec.Schema, error) {
@@ -22,16 +28,18 @@ func getSchema(target interface{}, defs spec.Definitions) (*spec.Schema, error) 
 func newSchemaRegistry() *schemaRegistry {
 	return &schemaRegistry{
 		mutex:   sync.RWMutex{},
-		storage: map[reflect.Type]func() (*spec.Schema, error){},
+		storage: map[reflect.Type]func(*schemaRegistry, interface{}) (*spec.Schema, error){},
+		kinds:   map[reflect.Kind]func(*schemaRegistry, interface{}) (*spec.Schema, error){},
 	}
 }
 
 type schemaRegistry struct {
 	mutex   sync.RWMutex
-	storage map[reflect.Type]func() (*spec.Schema, error)
+	storage map[reflect.Type]func(*schemaRegistry, interface{}) (*spec.Schema, error)
+	kinds   map[reflect.Kind]func(*schemaRegistry, interface{}) (*spec.Schema, error)
 }
 
-func (s *schemaRegistry) registerSchema(target interface{}, fn func() (*spec.Schema, error)) error {
+func (s *schemaRegistry) registerSchema(target interface{}, fn func(*schemaRegistry, interface{}) (*spec.Schema, error)) error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
@@ -43,8 +51,44 @@ func (s *schemaRegistry) registerSchema(target interface{}, fn func() (*spec.Sch
 	s.storage[typ] = fn
 	return nil
 }
+func (s *schemaRegistry) registerKind(target interface{}, fn func(*schemaRegistry, interface{}) (*spec.Schema, error)) error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	typ := reflect.TypeOf(target)
+
+	for typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+	}
+
+	s.kinds[typ.Kind()] = fn
+
+	return nil
+}
 
 func (s *schemaRegistry) getSchema(target interface{}, defs spec.Definitions) (*spec.Schema, error) {
-	panic("too not")
-	return schemaReg.getSchema(target, defs)
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	typ := reflect.TypeOf(target)
+	required := true
+	for typ.Kind() == reflect.Ptr {
+		required = false
+		typ = typ.Elem()
+	}
+
+	if found, ok := s.storage[typ]; ok {
+		result, err := found(s, target)
+		if err != nil {
+			return nil, err
+		}
+		result.Nullable = !required
+		return result, nil
+	}
+
+	if found, ok := s.kinds[typ.Kind()]; ok {
+		return found(s, target)
+	}
+
+	return nil, errSchemaValueNotFound
 }
--- schema_test.go
+++ schema_test.go
@@ -9,10 +9,52 @@ import (
 
 func TestSchema(t *testing.T) {
 	t.Run("test basic types in schema", func(t *testing.T) {
-		reg := newSchemaRegistry()
-		assert.Nil(t, reg.registerSchema(int(1), func() (*spec.Schema, error) {
-			return nil, nil
-		}))
+		data := []struct {
+			in         interface{}
+			schemaFunc func(interface{}) *spec.Schema
+			expect     func(t2 *testing.T, schema *spec.Schema)
+		}{
+			{int(1), func(interface{}) *spec.Schema {
+				return &spec.Schema{
+					SchemaProps: spec.SchemaProps{
+						Type: []string{"integer"},
+					},
+				}
+			}, func(t *testing.T, s *spec.Schema) {
+
+				// test here
+
+			}},
+		}
+
+		for _, item := range data {
+			reg := newSchemaRegistry()
+			assert.NoError(t, reg.registerSchema(item.in, func(*schemaRegistry, interface{}) (*spec.Schema, error) {
+				return item.schemaFunc(item.in), nil
+			}))
+			// check get
+			sch, err := reg.getSchema(item.in, make(spec.Definitions))
+			assert.NoError(t, err)
+			assert.NotNil(t, sch)
+			item.expect(t, sch)
+		}
 
 	})
+
+	t.Run("test kind", func(t *testing.T) {
+		reg := newSchemaRegistry()
+		assert.NoError(t, reg.registerKind(int(0), func(*schemaRegistry, interface{}) (*spec.Schema, error) {
+			return &spec.Schema{
+				SchemaProps: spec.SchemaProps{
+					Type: []string{"integer"},
+				},
+			}, nil
+		}))
+
+		sch, err := reg.getSchema(int(0), make(spec.Definitions))
+		assert.NoError(t, err)
+		assert.NotNil(t, sch)
+
+	})
+
 }
